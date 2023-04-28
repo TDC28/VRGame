35f84d1fb84bb67d0cb5d52a189a688b62dd9307
Montrelu <noah.montreuil@gmail.com>
2023-04-28 11:52:41 -0300

    Added doc

--- Assets/ExampleAssets/Scripts/Trap.cs
+++ Assets/ExampleAssets/Scripts/Trap.cs
@@ -17,12 +17,12 @@ public class Trap: MonoBehaviour {
 
     public void Start()
     {
-        
+        //Highly advise getting the components(rigidbody, colliders and everything to be able to apply forces and etc.)
     }
 
     public void Update()
     {
-        
+        //Check if trap is in water, if yes call commence. If it is coming out of water, use ReturnLobster to return the amount of lobsters, it should automatically shut of Commence()
     }
 
 
@@ -30,12 +30,12 @@ public class Trap: MonoBehaviour {
 
     
 
-    public void Commence()
+    public void Commence() //Start this function when the it touches water
     {
         this.isActive = true; //sets is active to true
         while (this.isActive == true) //starts a loop to randomize the amount of lobsters we get over time.
         {
-            Thread.Sleep(this.interval); //A problem might occure when trying to add sleeping to the game.
+            Thread.Sleep(this.interval); //A problem might occure when trying to add sleeping to the game
             double rng = rnd.NextDouble();
 
             if(lobsters == this.fullAmount)
@@ -55,7 +55,7 @@ public class Trap: MonoBehaviour {
         this.lobsters += 1;
     }
 
-    public int ReturnLobster()
+    public int ReturnLobster() //Use this function when removing the trap from the water
     {
         this.isActive = false;
         return this.lobsters;
